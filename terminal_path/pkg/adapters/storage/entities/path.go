0b94c4d6cedf9580be0a802fcf0d0137f2d59b75
Neda Hn <z.hosseini.7@gmail.com>
2024-07-21 17:36:24 +0330

    feat: terminal path entities and relationships

--- terminal_path/pkg/adapters/storage/entities/path.go
+++ terminal_path/pkg/adapters/storage/entities/path.go
@@ -1,1 +1,17 @@
 package entities
+
+import (
+
+	"gorm.io/gorm"
+)
+
+type Path struct {
+	gorm.Model
+	FromTerminalID uint          `gorm:"not null"`
+	ToTerminalID   uint          `gorm:"not null"`
+	FromTerminal   Terminal      `gorm:"foreignKey:FromTerminalID"`
+	ToTerminal     Terminal      `gorm:"foreignKey:ToTerminalID"`
+	Distance       float64       `gorm:"type:decimal(10,2);not null"` // in kilometers
+	Code           string        `gorm:"type:varchar(50);not null;uniqueIndex"`
+	Name           string        `gorm:"type:varchar(100)"`
+}
--- terminal_path/pkg/adapters/storage/entities/terminal.go
+++ terminal_path/pkg/adapters/storage/entities/terminal.go
@@ -1,1 +1,21 @@
 package entities
+
+import "gorm.io/gorm"
+
+type TerminalType string
+
+const (
+	Air TerminalType = "ait"
+	Rail TerminalType = "rail"
+	Road TerminalType = "road"
+	Sailing TerminalType = "sailing" // port
+)
+
+type Terminal struct{
+	gorm.Model
+	Name     string       `gorm:"type:varchar(100);not null;uniqueIndex"`
+	Type     TerminalType `gorm:"type:varchar(20);not null"`
+	City     string       `gorm:"type:varchar(100);not null"`
+	Country  string       `gorm:"type:varchar(100);not null"`
+	Paths    []Path       `gorm:"foreignKey:FromTerminalID"` 
+}
