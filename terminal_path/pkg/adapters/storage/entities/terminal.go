f828fc8d0cd63428ad76535361de6e886b8fb00e
Neda Hn <z.hosseini.7@gmail.com>
2024-07-24 15:58:09 +0330

    fix: unique together

--- terminal_path/pkg/adapters/storage/entities/terminal.go
+++ terminal_path/pkg/adapters/storage/entities/terminal.go
@@ -10,15 +10,15 @@ import (
 type Terminal struct {
 	gorm.Model
 	Name           string `gorm:"type:varchar(100);not null"`
-	NormalizedName string `gorm:"type:varchar(100);not null;index:idx_normalized_name_type_city_country,priority:1"`
-	Type           string `gorm:"type:varchar(20);not null;index:idx_normalized_name_type_city_country,priority:2"`
-	City           string `gorm:"type:varchar(100);not null;index:idx_normalized_name_type_city_country,priority:3"`
-	Country        string `gorm:"type:varchar(100);not null;index:idx_normalized_name_type_city_country,priority:4"`
+	NormalizedName string `gorm:"type:varchar(100);not null;index:idx_normalized_name_type_city_country,unique,priority:1"`
+	Type           string `gorm:"type:varchar(20);not null;index:idx_normalized_name_type_city_country,unique,priority:2"`
+	City           string `gorm:"type:varchar(100);not null;index:idx_normalized_name_type_city_country,unique,priority:3"`
+	Country        string `gorm:"type:varchar(100);not null;index:idx_normalized_name_type_city_country,unique,priority:4"`
 	OutgoingPaths  []Path `gorm:"foreignKey:FromTerminalID"`
 	IncomingPaths  []Path `gorm:"foreignKey:ToTerminalID"`
 
 	// Composite unique constraint
-	UniqueTerminal string `gorm:"uniqueIndex:idx_normalized_name_type_city_country"`
+	//UniqueTerminal string `gorm:"uniqueIndex:idx_normalized_name_type_city_country"`
 }
 
 func NormalizeName(name string) string {
