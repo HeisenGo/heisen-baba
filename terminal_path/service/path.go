ca52a68e890cb5d66195ed9b838ca93ee5cd6240
Neda Hn <z.hosseini.7@gmail.com>
2024-07-26 01:22:08 +0330

    feat: services of getting full path, patching and deleting a path

--- terminal_path/service/path.go
+++ terminal_path/service/path.go
@@ -42,3 +42,44 @@ func (s *PathService) CreatePath(ctx context.Context, path *path.Path) error {
 func (s *PathService) GetPathsByOriginDestinationType(ctx context.Context, originCity, destinationCity, pathType string, page, pageSize uint) ([]path.Path, uint, error) {
 	return s.pathOps.GetPathsByOriginDestinationType(ctx, originCity, destinationCity, pathType, page, pageSize)
 }
+
+func (s *PathService) GetFullPathByID(ctx context.Context, id uint) (*path.Path, error) {
+	return s.pathOps.GetFullPathByID(ctx, id)
+}
+
+func (s *PathService) PatchPath(ctx context.Context, updatedPath *path.Path, hasUnfinishedTrips bool) (*path.Path, error) {
+	originalPath, err := s.GetFullPathByID(ctx, updatedPath.ID)
+	if err != nil {
+		return nil, err
+	}
+	if updatedPath.FromTerminalID != 0 {
+		updatedPath.FromTerminal, err = s.terminalOps.GetTerminalByID(ctx, updatedPath.FromTerminalID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if updatedPath.ToTerminalID != 0 {
+		updatedPath.ToTerminal, err = s.terminalOps.GetTerminalByID(ctx, updatedPath.ToTerminalID)
+		if err != nil {
+			return nil, err
+		}
+	}
+	err = s.pathOps.PatchPath(ctx, updatedPath, originalPath, hasUnfinishedTrips)
+	if err != nil {
+		return nil, err
+	}
+	return originalPath, nil
+}
+
+func (s *PathService) DeletePath(ctx context.Context, pathID uint, hasUnFinishedTrips bool) (*path.Path, error) {
+	p, err := s.pathOps.GetFullPathByID(ctx, pathID)
+	if err != nil {
+		return nil, err
+	}
+	err = s.pathOps.Delete(ctx, pathID, hasUnFinishedTrips)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
