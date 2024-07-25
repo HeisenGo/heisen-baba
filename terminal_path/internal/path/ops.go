8cb9a64de625990d1f56072e32dcef1e457621c9
Neda Hn <z.hosseini.7@gmail.com>
2024-07-26 01:19:20 +0330

    feat: patch and delete path ops and check validation for code path

--- terminal_path/internal/path/ops.go
+++ terminal_path/internal/path/ops.go
@@ -22,6 +22,9 @@ func (o *Ops) Create(ctx context.Context, path *Path) error {
 	if err := internal.ValidateName(path.Name, MaxStringLength); err != nil {
 		return err
 	}
+	if err := internal.ValidateName(path.Code, MaxCodeLength); err != nil {
+		return err
+	}
 	if err := path.ValidateStartAndEndCities(); err != nil {
 		return err
 	}
@@ -50,4 +53,90 @@ func (o *Ops) GetPathsByOriginDestinationType(ctx context.Context, originCity, d
 
 	return terminals, total, err
 
+}
+
+func (o *Ops) GetFullPathByID(ctx context.Context, id uint) (*Path, error) {
+	p, err := o.repo.GetFullPathByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if p == nil {
+		return nil, ErrPathNotFound
+	}
+
+	return p, nil
+
+}
+
+func (o *Ops) PatchPath(ctx context.Context, updatedPath, originalPath *Path, hasUnFinishedTrip bool) error {
+	if hasUnFinishedTrip {
+		if updatedPath.FromTerminalID != uint(0) || updatedPath.ToTerminalID != uint(0) {
+			return ErrCanNotUpdatePath
+		}
+	} else {
+		if updatedPath.FromTerminalID != uint(0) && updatedPath.ToTerminalID == uint(0) {
+			updatedPath.ToTerminal = originalPath.ToTerminal
+			updatedPath.ToTerminalID = originalPath.ToTerminalID
+			err := updatedPath.ValidateStartEndTerminalTypes()
+			if err != nil {
+				return err
+			}
+			err = updatedPath.ValidateStartAndEndCities()
+			if err != nil {
+				return err
+			}
+			originalPath.FromTerminal = updatedPath.FromTerminal
+			originalPath.FromTerminalID = updatedPath.FromTerminalID
+		}
+
+		if updatedPath.FromTerminalID == uint(0) && updatedPath.ToTerminalID != uint(0) {
+			updatedPath.FromTerminal = originalPath.FromTerminal
+			updatedPath.FromTerminalID = originalPath.FromTerminalID
+			err := updatedPath.ValidateStartEndTerminalTypes()
+			if err != nil {
+				return err
+			}
+			err = updatedPath.ValidateStartAndEndCities()
+			if err != nil {
+				return err
+			}
+			originalPath.ToTerminal = updatedPath.ToTerminal
+			originalPath.ToTerminalID = updatedPath.ToTerminalID
+		}
+		if updatedPath.FromTerminalID != uint(0) && updatedPath.ToTerminalID != uint(0) {
+			err := updatedPath.ValidateStartEndTerminalTypes()
+			if err != nil {
+				return err
+			}
+			err = updatedPath.ValidateStartAndEndCities()
+			if err != nil {
+				return err
+			}
+			updatedPath.Type = updatedPath.FromTerminal.Type
+			originalPath.FromTerminal = updatedPath.FromTerminal
+			originalPath.FromTerminalID = updatedPath.FromTerminalID
+			originalPath.ToTerminal = updatedPath.ToTerminal
+			originalPath.ToTerminalID = updatedPath.ToTerminalID
+
+		}
+	}
+	if updatedPath.Name != "" {
+		if err := internal.ValidateName(updatedPath.Name, MaxStringLength); err != nil {
+			return err
+		}
+	}
+	if updatedPath.Code != ""{
+		if err := internal.ValidateName(updatedPath.Code, MaxCodeLength); err != nil {
+			return err
+		}
+	}
+	return o.repo.PatchPath(ctx, updatedPath, originalPath)
+}
+
+func (o *Ops) Delete(ctx context.Context, pathID uint, hasUnfinishedTrip bool) error {
+	if hasUnfinishedTrip {
+		return ErrCanNotDelete
+	}
+	return o.repo.Delete(ctx, pathID)
 }
