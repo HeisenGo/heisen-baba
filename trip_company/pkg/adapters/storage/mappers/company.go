718bea70c2fc32c329fbb36113ede42bc0874924
Neda Hn <z.hosseini.7@gmail.com>
2024-07-27 19:03:14 +0330

    feat: inserting company query added

--- trip_company/pkg/adapters/storage/mappers/company.go
+++ trip_company/pkg/adapters/storage/mappers/company.go
@@ -1,1 +1,34 @@
 package mappers
+
+import (
+	"tripcompanyservice/internal/company"
+	"tripcompanyservice/pkg/adapters/storage/entities"
+	"tripcompanyservice/pkg/fp"
+)
+
+func CompanyEntityToDomain(companyEntity entities.TransportCompany) company.TransportCompany {
+	return company.TransportCompany{
+		ID:          companyEntity.ID,
+		Name:        companyEntity.Name,
+		Description: companyEntity.Description,
+		OwnerID:     companyEntity.OwnerID,
+		Address:     companyEntity.Address,
+		PhoneNumber: companyEntity.PhoneNumber,
+		Email:       companyEntity.Email,
+	}
+}
+
+func CompanyEntitiesToDomain(companyEntities []entities.TransportCompany) []company.TransportCompany {
+	return fp.Map(companyEntities, CompanyEntityToDomain)
+}
+
+func CompanyDomainToEntity(c *company.TransportCompany) *entities.TransportCompany {
+	return &entities.TransportCompany{
+		Name:        c.Name,
+		Description: c.Description,
+		OwnerID:     c.OwnerID,
+		Address:     c.Address,
+		PhoneNumber: c.PhoneNumber,
+		Email:       c.Email,
+	}
+}
