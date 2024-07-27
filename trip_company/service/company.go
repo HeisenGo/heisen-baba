540660fca96c05bd3e2d1857ec67a4f45e64d83c
Neda Hn <z.hosseini.7@gmail.com>
2024-07-27 19:04:39 +0330

    feat: inserting company service added

--- trip_company/service/company.go
+++ trip_company/service/company.go
@@ -1,1 +1,20 @@
 package service
+
+import (
+	"context"
+	"tripcompanyservice/internal/company"
+)
+
+type TransportCompanyService struct {
+	companyOps *company.Ops
+}
+
+func NewTransportCompanyService(companyOps *company.Ops) *TransportCompanyService {
+	return &TransportCompanyService{
+		companyOps: companyOps,
+	}
+}
+
+func (s *TransportCompanyService) CreateTransportCompany(ctx context.Context, company *company.TransportCompany) error {
+	return s.companyOps.Create(ctx, company)
+}
