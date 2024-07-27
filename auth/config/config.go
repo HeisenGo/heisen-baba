247dbb195056f253f46ab4b38ad79cd1ccf404e2
Neda Hn <z.hosseini.7@gmail.com>
2024-07-28 00:39:24 +0330

    feat: add to consul

--- auth/config/config.go
+++ auth/config/config.go
@@ -7,11 +7,18 @@ type Config struct {
 }
 
 type Server struct {
-	HTTPPort               int    `mapstructure:"http_port"`
-	Host                   string `mapstructure:"host"`
-	TokenExpMinutes        uint   `mapstructure:"token_exp_minutes"`
-	RefreshTokenExpMinutes uint   `mapstructure:"refresh_token_exp_minutes"`
-	TokenSecret            string `mapstructure:"token_secret"`
+	HTTPPort               int             `mapstructure:"http_port"`
+	Host                   string          `mapstructure:"host"`
+	TokenExpMinutes        uint            `mapstructure:"token_exp_minutes"`
+	RefreshTokenExpMinutes uint            `mapstructure:"refresh_token_exp_minutes"`
+	TokenSecret            string          `mapstructure:"token_secret"`
+	ServiceRegistry        ServiceRegistry `mapstructure:"service_registry"`
+	ServiceHostName        string          `mapstructure:"service_host_name"`
+	ServiceHTTPHealthPath  string          `mapstructure:"service_http_health_path"`
+	ServiceHTTPPrefixPath  string          `mapstructure:"service_http_prefix_path"`
+}
+type ServiceRegistry struct {
+	Address string `mapstructure:"address"`
 }
 
 type DB struct {
