cb3963adb4937100ddf41a66372bcf84caa210a8
Neda Hn <z.hosseini.7@gmail.com>
2024-07-29 18:52:36 +0330

    feat: is_blocked field added

--- trip_company/pkg/adapters/storage/entities/transport_company.go
+++ trip_company/pkg/adapters/storage/entities/transport_company.go
@@ -6,12 +6,13 @@ import (
 
 type TransportCompany struct {
 	gorm.Model
-	Name        string `gorm:"type:varchar(100);not null"`
+	Name        string `gorm:"type:varchar(100);not null;uniqueIndex:idx_owner_name"`
 	Description string `gorm:"type:text"`
-	OwnerID     uint   `gorm:"not null"`
+	OwnerID     uint   `gorm:"not null;uniqueIndex:idx_owner_name"`
 	Address     string `gorm:"type:varchar(255)"`
 	PhoneNumber string `gorm:"type:varchar(20)"`
 	Email       string `gorm:"type:varchar(100);uniqueIndex"`
+	IsBlocked   bool   `gorm:"not null; default:false"`
 	// relationships
 	Employees   []Employee `gorm:"foreignKey:TransportCompanyID"`
 	Trips       []Trip `gorm:"foreignKey:TransportCompanyID"`
