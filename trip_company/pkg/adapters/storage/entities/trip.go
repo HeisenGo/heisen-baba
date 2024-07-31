3b93b31ad744e55db72a334e2220017c0ec76af4
Neda Hn <z.hosseini.7@gmail.com>
2024-08-01 01:57:45 +0330

    fix: trip entity updated

--- trip_company/pkg/adapters/storage/entities/trip.go
+++ trip_company/pkg/adapters/storage/entities/trip.go
@@ -6,35 +6,34 @@ import (
 	"gorm.io/gorm"
 )
 
-type TripType string
-
-const (
-	RailTrip    TripType = "rail"
-	AirTrip     TripType = "air"
-	RoadTrip    TripType = "road"
-	SailingTrip TripType = "sailing"
-)
-
 type Trip struct {
 	gorm.Model
-	TransportCompanyID    uint
-	TransportCompany      TransportCompany `gorm:"foreignKey:TransportCompanyID"`
-	TripType              TripType `gorm:"type:varchar(20);not null"`
-	UserReleaseDate       time.Time
-	TourReleaseDate       time.Time
-	Price                 float64
-	PathID                uint   `gorm:"not null"`
-	Origin                string `gorm:"type:varchar(100);not null"`
-	Destination           string `gorm:"type:varchar(100);not null"`
-	MinPassengers         int
-	VehicleReservationFee float64
-	VehicleProductionYear int
-	Status                string `gorm:"type:varchar(20);default:'pending'"`
-	TechTeamID            uint
-	TechTeam              TechTeam `gorm:"foreignKey:TechTeamID"`
-	VehicleRequestID      uint
-	VehicleRequest        VehicleRequest `gorm:"foreignKey:TripID"`
-	Tickets               []Ticket `gorm:"foreignKey:TripID"`
+	TransportCompanyID      uint `gorm:"not null;uniqueIndex:idx_trip_unique"`
+	TransportCompany        *TransportCompany `gorm:"foreignKey:TransportCompanyID"`
+	TripType                string            `gorm:"type:varchar(20);not null"`
+	UserReleaseDate         time.Time
+	TourReleaseDate         time.Time
+	UserPrice               float64
+	AgencyPrice             float64
+	PathID                  uint   `gorm:"not null;uniqueIndex:idx_trip_unique"`
+	Origin                  string `gorm:"type:varchar(100);not null"`
+	FromTerminalName        string
+	ToTerminalName          string
+	Destination             string `gorm:"type:varchar(100);not null"`
+	PathName                string
+	Status                  string `gorm:"type:varchar(20);default:'pending'"`
+	MinPassengers           uint
+	TechTeamID              *uint     `gorm:""`
+	TechTeam                *TechTeam `gorm:"foreignKey:TechTeamID"`
+	VehicleRequestID        *uint
+	VehicleRequest          *VehicleRequest `gorm:"foreignKey:TripID"`
+	Tickets                 []Ticket        `gorm:"foreignKey:TripID"`
+	TripCancellingPenaltyID *uint
+	TripCancelingPenalty    *TripCancellingPenalty `gorm:"foreignKey:TripCancellingPenaltyID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	MaxTickets              uint
+	VehicleID               *uint
+	IsCanceled              bool       `gorm:"default:false"`
+	IsFinished              bool       `gorm:"default:false"`
+	StartDate               *time.Time `gorm:"uniqueIndex:idx_trip_unique"` // should be given by trip generator
+	EndDate                 *time.Time // should be calculated according to the vehicle speed and path distance
 }
-
-
