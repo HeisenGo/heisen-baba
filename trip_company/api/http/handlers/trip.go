e1450deecadf81644068392563a6b5852c12b3d2
Neda Hn <z.hosseini.7@gmail.com>
2024-08-02 20:19:45 +0330

    feat: handlers to get trips according to the role of requester needs to be updated

--- trip_company/api/http/handlers/trip.go
+++ trip_company/api/http/handlers/trip.go
@@ -1,13 +1,16 @@
 package handlers
 
 import (
+	"errors"
+	"time"
 	"tripcompanyservice/api/http/handlers/presenter"
+	"tripcompanyservice/internal/trip"
 	"tripcompanyservice/service"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func CreateTrip(tripService *service.TripService)fiber.Handler {//serviceFactory ServiceFactory[*service.TripService])fiber.Handler {
+func CreateTrip(tripService *service.TripService) fiber.Handler { //serviceFactory ServiceFactory[*service.TripService])fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		//tripService := serviceFactory(c.UserContext())
 
@@ -22,7 +25,6 @@ func CreateTrip(tripService *service.TripService)fiber.Handler {//serviceFactory
 			return presenter.BadRequest(c, err)
 		}
 
-		
 		//userClaims, ok := c.Locals(UserClaimKey).(*jwt.UserClaims)
 		// if !ok {
 		// 	return SendError(c, errWrongClaimType, fiber.StatusBadRequest)
@@ -39,8 +41,85 @@ func CreateTrip(tripService *service.TripService)fiber.Handler {//serviceFactory
 			// apply trace ID here .... TODO
 			return presenter.InternalServerError(c, err)
 		}
-
-		res := presenter.TripToCreateTripReq(t)
+		res := presenter.TripToOwnerAdminTechTeamOperatorTripResponse(*t)
 		return presenter.Created(c, "Trip created successfully", res)
+	}
+}
+
+func GetFullTripByID(tripService *service.TripService) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// userClaims, ok := c.Locals(UserClaimKey).(*jwt.UserClaims)
+		// if !ok {
+		// 	return SendError(c, errWrongClaimType, fiber.StatusBadRequest)
+		// }
+		//query parameter
+		tripID, err := c.ParamsInt("tripID")
+		if err != nil {
+			return presenter.BadRequest(c, errWrongIDType)
+		}
+
+		if tripID < 0 {
+			return presenter.BadRequest(c, errWrongIDType)
+		}
+		// requester!!!!!!!!!!! check what to show
+		requester := "owner" // "user" // "admin" // "operator" // "employee" // "techteam"
+		t, err := tripService.GetFullTripByID(c.UserContext(), uint(tripID))
+		if err != nil {
+			if errors.Is(err, trip.ErrRecordNotFound) {
+				return presenter.BadRequest(c, err)
+			}
+			err := errors.New("Error")
+			return presenter.InternalServerError(c, err)
+		}
+		var data interface{}
+		if requester == "owner" {
+			data = presenter.TripToOwnerAdminTechTeamOperatorTripResponse(*t)
+		}
+		// TO DO implement else
+		return presenter.OK(c, "Trip fetched successfully", data)
+	}
+}
+
+func GetTrips(tripService *service.TripService) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// userClaims, ok := c.Locals(UserClaimKey).(*jwt.UserClaims)
+		// if !ok {
+		// 	return SendError(c, errWrongClaimType, fiber.StatusBadRequest)
+		// }
+		//query parameter
+		page, pageSize := PageAndPageSize(c)
+		startDateStr := c.Query("start_date")
+		originCity := c.Query("from")
+		destinationCity := c.Query("to")
+		pathType := c.Query("type")
+		requesterType := c.Query("requester_type") // get from auth!!!!! TODO:
+
+		// Parse dates
+		var startDate time.Time
+		var err error
+
+		if startDateStr != "" {
+			startDate, err = time.Parse("2006-01-02", startDateStr)
+			if err != nil {
+				return presenter.BadRequest(c, errors.New("invalid start date format"))
+			}
+		}
+		//startDateStr := startDate.Format("2006-01-02") // Convert to YYYY-MM-DD
+
+		trips, total, err := tripService.GetTrips(c.UserContext(), originCity, destinationCity, pathType, &startDate, requesterType, uint(page), uint(pageSize))
+		if err != nil {
+			if errors.Is(err, trip.ErrRecordsNotFound) {
+				return presenter.BadRequest(c, err)
+			}
+			err := errors.New("Error")
+			return presenter.InternalServerError(c, err)
+		}
+		data := presenter.NewPagination(
+			presenter.BatchTripToOwnerAdminTechTeamOperatorTripResponse(trips),
+			uint(page),
+			uint(pageSize),
+			total,
+		)
+		return presenter.OK(c, "Trips fetched successfully", data)
 	}
 }
