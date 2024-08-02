f48bb619ec4449912253f8673a7908d3706d4b7c
Neda Hn <z.hosseini.7@gmail.com>
2024-08-02 20:23:06 +0330

    feat: query to get by id penalty

--- trip_company/pkg/adapters/storage/trip_cancelling_penalty.go
+++ trip_company/pkg/adapters/storage/trip_cancelling_penalty.go
@@ -1,50 +1,33 @@
 package storage
 
-// import (
-// 	"context"
-// 	"strings"
-// 	"tripcompanyservice/internal/trip"
-// 	tripcancellingpenalty "tripcompanyservice/internal/trip_cancelling_penalty"
-// 	"tripcompanyservice/pkg/adapters/storage/mappers"
+import (
+	"context"
+	"strings"
+	tripcancellingpenalty "tripcompanyservice/internal/trip_cancelling_penalty"
+	"tripcompanyservice/pkg/adapters/storage/entities"
+	"tripcompanyservice/pkg/adapters/storage/mappers"
 
-// 	"gorm.io/gorm"
-// )
+	"gorm.io/gorm"
+)
 
-// type tripCancellingPenaltyRepo struct {
-// 	db *gorm.DB
-// }
+type tripCancellingPenaltyRepo struct {
+	db *gorm.DB
+}
 
-// func NewTripCancellingPenaltyRepo(db *gorm.DB) tripcancellingpenalty.Repo {
-// 	return &tripCancellingPenaltyRepo{db}
-// }
+func NewTripCancellingPenaltyRepo(db *gorm.DB) tripcancellingpenalty.Repo {
+	return &tripCancellingPenaltyRepo{db}
+}
 
-// func (r *tripCancellingPenaltyRepo) Insert(ctx context.Context, p *tripcancellingpenalty.TripCancelingPenalty) error {
-// 	penaltyEntity := mappers.PenaltyDomainToEntity(p)
+func (r *tripCancellingPenaltyRepo) GetByID(ctx context.Context, id uint) (*tripcancellingpenalty.TripCancelingPenalty, error) {
+	var t entities.TripCancellingPenalty
 
-// 	result := r.db.WithContext(ctx).Save(&penaltyEntity)
-// 	if result.Error != nil {
-// 		if strings.Contains(result.Error.Error(), "duplicate key") {
-
-// 			// var existingTerminal entities.Trip
-// 			// // Search for the soft-deleted record with the same unique constraints
-// 			// if r.db.WithContext(ctx).Unscoped().Where("name = ? AND type = ? AND city = ? AND country = ?", t.Name, t.Type, t.City, t.Country).First(&existingTerminal).Error == nil {
-// 			// 	// Check if the record is soft-deleted
-// 			// 	if existingTerminal.DeletedAt.Valid {
-// 			// 		// Restore the soft-deleted record
-// 			// 		existingTerminal.DeletedAt = gorm.DeletedAt{}
-// 			// 		if err := r.db.WithContext(ctx).Save(&existingTerminal).Error; err != nil {
-// 			// 			return fmt.Errorf("%w %w", terminal.ErrFailedToRestore, err)
-// 			// 		}
-// 			// 		t.ID = existingTerminal.ID
-// 			// 		return nil
-// 			// 	}
-
-// 			return trip.ErrDuplication
-// 		}
-// 		return result.Error
-
-// 	}
-// 	p.ID = penaltyEntity.ID
-
-// 	return nil
-// }
+	err := r.db.WithContext(ctx).Model(&entities.TripCancellingPenalty{}).Where("id = ?", id).First(&t).Error
+	if err != nil {
+		if strings.Contains(err.Error(), "record not found") {
+			return nil, nil
+		}
+		return nil, err
+	}
+	dC := mappers.PenaltyEntityToDomain(t)
+	return &dC, nil
+}
