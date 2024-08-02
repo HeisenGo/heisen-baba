8da0fc51b1fc97f9f84d0ce4cce9709cde0aa969
Neda Hn <z.hosseini.7@gmail.com>
2024-08-02 20:20:26 +0330

    feat: presenter for showing res for penalty added

--- trip_company/api/http/handlers/presenter/trip_cancelling_penalty.go
+++ trip_company/api/http/handlers/presenter/trip_cancelling_penalty.go
@@ -35,3 +35,33 @@ func TripCancelingPenaltyToTripCancellingPenaltyReq(tCp *tripcancellingpenalty.T
 		ThirdCancellationPercentage:  tCp.ThirdCancellationPercentage,
 	}
 }
+
+type CreateTripCancelingPenaltyRes struct {
+	ID                           uint      `json:"id"`
+	FirstDays                    uint      `json:"first_days"`
+	FirstDate                    Timestamp `json:"first_date"`
+	FirstCancellationPercentage  uint      `json:"first_percentage"`
+	SecondDays                   uint      `json:"second_days"`
+	SecondDate                   Timestamp `json:"second_date"`
+	SecondCancellationPercentage uint      `json:"second_percentage"`
+	ThirdCancellationPercentage  uint      `json:"third_percentage" `
+	ThirdDays                    uint      `json:"third_days"`
+	ThirdDate                    Timestamp `json:"third_date"`
+}
+
+func TripCancelingPenaltyToTripCancellingPenaltyRes(tCp *tripcancellingpenalty.TripCancelingPenalty) *CreateTripCancelingPenaltyRes {
+	return &CreateTripCancelingPenaltyRes{
+		ID:                          tCp.ID,
+		FirstDays:                   tCp.FirstDays,
+		FirstDate:                   Timestamp(tCp.FirstDate),
+		FirstCancellationPercentage: tCp.FirstCancellationPercentage,
+		SecondDays:                  tCp.SecondDays,
+		SecondDate:                  Timestamp(tCp.SecondDate),
+
+		SecondCancellationPercentage: tCp.SecondCancellationPercentage,
+		ThirdDays:                    tCp.ThirdDays,
+		ThirdDate:                    Timestamp(tCp.ThirdDate),
+
+		ThirdCancellationPercentage: tCp.ThirdCancellationPercentage,
+	}
+}
