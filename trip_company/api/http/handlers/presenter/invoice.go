127a2a3f30219f63a2127376f48043b5b99d9705
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 03:57:58 +0330

    feat: presenters to get invoice different for agency and user

---
+++ trip_company/api/http/handlers/presenter/invoice.go
@@ -0,0 +1,49 @@
+package presenter
+
+import "tripcompanyservice/internal/invoice"
+
+type AgencyInvoice struct {
+	ID             uint         `json:"id"`
+	TicketID       uint         `json:"ticket_id"`
+	Ticket         AgencyTicket `json:"ticket"`
+	IssuedDate     Timestamp    `json:"issued_date"`
+	Info           string       `json:"info"`
+	PerAmountPrice float64      `json:"per_price"`
+	TotalPrice     float64      `json:"total_price"`
+}
+
+type UserInvoice struct {
+	ID             uint       `json:"id"`
+	TicketID       uint       `json:"ticket_id"`
+	Ticket         UserTicket `json:"ticket"`
+	IssuedDate     Timestamp  `json:"issued_date"`
+	Info           string     `json:"info"`
+	PerAmountPrice float64    `json:"per_price"`
+	TotalPrice     float64    `json:"total_price"`
+}
+
+func InvoiceToAgencyInvoice(i invoice.Invoice) AgencyInvoice {
+	ticket := TicketToAgencyTicket(i.Ticket)
+	return AgencyInvoice{
+		ID:             i.ID,
+		TicketID:       i.TicketID,
+		Ticket:         ticket,
+		IssuedDate:     Timestamp(i.IssuedDate),
+		Info:           i.Info,
+		PerAmountPrice: i.PerAmountPrice,
+		TotalPrice:     i.TotalPrice,
+	}
+}
+
+func InvoiceToUserInvoice(i invoice.Invoice) UserInvoice {
+	ticket := TicketToUserTicket(i.Ticket)
+	return UserInvoice{
+		ID:             i.ID,
+		TicketID:       i.TicketID,
+		Ticket:         ticket,
+		IssuedDate:     Timestamp(i.IssuedDate),
+		Info:           i.Info,
+		PerAmountPrice: i.PerAmountPrice,
+		TotalPrice:     i.TotalPrice,
+	}
+}
