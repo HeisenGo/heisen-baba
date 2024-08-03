ce86909449857fb585c41887d01513cbaaf931e2
Neda Hn <z.hosseini.7@gmail.com>
2024-08-04 02:45:39 +0330

    feat: model updated alongside some error handlings

--- trip_company/internal/ticket/types.go
+++ trip_company/internal/ticket/types.go
@@ -2,13 +2,22 @@ package ticket
 
 import (
 	"context"
+	"errors"
 	"tripcompanyservice/internal/trip"
+)
+
+var(
+    ErrRecordNotFound = errors.New("ticket not found")
+    ErrFailedToGetTicket = errors.New("failed to ge ticket")
+    ErrFailedToUpdate = errors.New("failed to update ticket")
 )
 
 type Repo interface {
 	Insert(ctx context.Context, t *Ticket) error
     UpdateTicketStatus(ctx context.Context, ticketID uint, status string) error
     GetTicketsByUserOrAgency(ctx context.Context, userID *uint, agencyID *uint, limit, offset uint) ([]Ticket,uint, error) 
+    GetFullTicketByID(ctx context.Context, id uint) (*Ticket, error)
+    UpdateTicket(ctx context.Context, id uint, updates map[string]interface{}) error
 }
 
 type Ticket struct {
@@ -20,4 +29,5 @@ type Ticket struct {
 	Quantity   int
 	TotalPrice float64
 	Status     string
+    Penalty    float64
 }
