5a70907a52c5d6f180e4f3ddd5c9f6f25ae6818a
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 22:36:06 +0330

    feat: get paginated invoices and update invoice queries

--- trip_company/pkg/adapters/storage/invoice.go
+++ trip_company/pkg/adapters/storage/invoice.go
@@ -32,7 +32,44 @@ func (r *invoiceRepo) Insert(ctx context.Context, i *invoice.Invoice) error {
 
 }
 
-func  (r *invoiceRepo)UpdateInvoiceStatus(ctx context.Context, invoiceID uint, status string) error {
-    // Ensure status is valid, e.g., 'paid', 'pending', etc.
-    return r.db.WithContext(ctx).Model(&entities.Invoice{}).Where("id = ?", invoiceID).Update("status", status).Error
+func (r *invoiceRepo) UpdateInvoiceStatus(ctx context.Context, invoiceID uint, status string) error {
+	// Ensure status is valid, e.g., 'paid', 'pending', etc.
+	return r.db.WithContext(ctx).Model(&entities.Invoice{}).Where("id = ?", invoiceID).Update("status", status).Error
+}
+
+func (r *invoiceRepo) GetInvoicesByUserOrAgency(ctx context.Context, userID *uint, agencyID *uint, limit, offset uint) ([]invoice.Invoice, uint,error) {
+	query := r.db.WithContext(ctx).Model(&entities.Invoice{}).
+		Preload("Ticket").
+		Preload("Ticket.Trip")
+
+		if userID != nil {
+			query = query.Joins("JOIN tickets ON tickets.id = invoices.ticket_id").
+				Where("tickets.user_id = ?", userID)
+		}
+		if agencyID != nil {
+			query = query.Joins("JOIN tickets ON tickets.id = invoices.ticket_id").
+				Where("tickets.agency_id = ?", agencyID)
+		}
+
+		var total int64
+
+		if err := query.Count(&total).Error; err != nil {
+			return nil, 0, err
+		}
+	
+		if offset > 0 {
+			query = query.Offset(int(offset))
+		}
+	
+		if limit > 0 {
+			query = query.Limit(int(limit))
+		}
+	
+		var invoices []entities.Invoice
+	
+		if err := query.Find(&invoices).Error; err != nil {
+			return nil, 0, err
+		}
+	dInvoices := mappers.InvoiceEntitiesToDomain(invoices)
+	return dInvoices, uint(total), nil
 }
