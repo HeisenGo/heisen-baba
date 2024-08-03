bfb7dd44f9f14c2008817394bca62bfca67c30f8
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 22:36:28 +0330

    feat: get paginated tickets and update ticket queries

--- trip_company/pkg/adapters/storage/ticket.go
+++ trip_company/pkg/adapters/storage/ticket.go
@@ -30,6 +30,40 @@ func (r *ticketRepo) Insert(ctx context.Context, t *ticket.Ticket) error {
 
 }
 
-func (r *ticketRepo)  UpdateTicketStatus(ctx context.Context, ticketID uint, status string) error {
-    return r.db.WithContext(ctx).Model(&entities.Ticket{}).Where("id = ?", ticketID).Update("status", status).Error
+func (r *ticketRepo) UpdateTicketStatus(ctx context.Context, ticketID uint, status string) error {
+	return r.db.WithContext(ctx).Model(&entities.Ticket{}).Where("id = ?", ticketID).Update("status", status).Error
+}
+
+func (r *ticketRepo) GetTicketsByUserOrAgency(ctx context.Context, userID *uint, agencyID *uint, limit, offset uint) ([]ticket.Ticket, uint, error) {
+	query := r.db.WithContext(ctx).Model(&entities.Ticket{}).
+		Preload("Trip")
+
+	if userID != nil {
+		query = query.Where("user_id = ?", userID)
+	}
+	if agencyID != nil {
+		query = query.Where("agency_id = ?", agencyID)
+	}
+
+	var total int64
+
+	if err := query.Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	if offset > 0 {
+		query = query.Offset(int(offset))
+	}
+
+	if limit > 0 {
+		query = query.Limit(int(limit))
+	}
+
+	var tickets []entities.Ticket
+
+	if err := query.Find(&tickets).Error; err != nil {
+		return nil, 0, err
+	}
+	dTickets := mappers.TicketEntitiesToDomain(tickets)
+	return dTickets, uint(total), nil
 }
