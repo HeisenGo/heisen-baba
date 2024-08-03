e57ef9ea73f974949f47c9f2bf77f13d87945296
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 04:01:08 +0330

    fix: ticket entity

--- trip_company/pkg/adapters/storage/entities/ticket.go
+++ trip_company/pkg/adapters/storage/entities/ticket.go
@@ -2,13 +2,13 @@ package entities
 
 import "gorm.io/gorm"
 
-type Ticket struct { // Renamed from TripPurchase to Ticket
-	gorm.Model
-	TripID     uint
-	Trip       Trip `gorm:"foreignKey:TripID"`
-	UserID     uint `gorm:"not null"`
-	TourID     uint
-	Quantity   int
-	TotalPrice float64
-	Status     string `gorm:"type:varchar(20);default:'pending'"`
+type Ticket struct {
+    gorm.Model
+    TripID     uint            `gorm:"not null"`
+    Trip       Trip            `gorm:"foreignKey:TripID"`
+    UserID     *uint           `gorm:"default:NULL"` // Use `default:NULL` for nullable field
+    AgencyID   *uint           `gorm:"default:NULL"`
+    Quantity   int
+    TotalPrice float64
+    Status     string          `gorm:"type:varchar(20);default:'pending'"`
 }
