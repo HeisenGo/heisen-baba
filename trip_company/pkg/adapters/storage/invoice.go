4c4e57b8f5224784a2e1f4875ccea1279e1a989d
Neda Hn <z.hosseini.7@gmail.com>
2024-08-04 02:47:13 +0330

    feat: queries to get full entity and update

--- trip_company/pkg/adapters/storage/invoice.go
+++ trip_company/pkg/adapters/storage/invoice.go
@@ -2,6 +2,8 @@ package storage
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"tripcompanyservice/internal/invoice"
 	"tripcompanyservice/pkg/adapters/storage/entities"
 	"tripcompanyservice/pkg/adapters/storage/mappers"
@@ -37,39 +39,66 @@ func (r *invoiceRepo) UpdateInvoiceStatus(ctx context.Context, invoiceID uint, s
 	return r.db.WithContext(ctx).Model(&entities.Invoice{}).Where("id = ?", invoiceID).Update("status", status).Error
 }
 
-func (r *invoiceRepo) GetInvoicesByUserOrAgency(ctx context.Context, userID *uint, agencyID *uint, limit, offset uint) ([]invoice.Invoice, uint,error) {
+func (r *invoiceRepo) GetInvoicesByUserOrAgency(ctx context.Context, userID *uint, agencyID *uint, limit, offset uint) ([]invoice.Invoice, uint, error) {
 	query := r.db.WithContext(ctx).Model(&entities.Invoice{}).
 		Preload("Ticket").
 		Preload("Ticket.Trip")
 
-		if userID != nil {
-			query = query.Joins("JOIN tickets ON tickets.id = invoices.ticket_id").
-				Where("tickets.user_id = ?", userID)
-		}
-		if agencyID != nil {
-			query = query.Joins("JOIN tickets ON tickets.id = invoices.ticket_id").
-				Where("tickets.agency_id = ?", agencyID)
-		}
+	if userID != nil {
+		query = query.Joins("JOIN tickets ON tickets.id = invoices.ticket_id").
+			Where("tickets.user_id = ?", userID)
+	}
+	if agencyID != nil {
+		query = query.Joins("JOIN tickets ON tickets.id = invoices.ticket_id").
+			Where("tickets.agency_id = ?", agencyID)
+	}
 
-		var total int64
+	var total int64
 
-		if err := query.Count(&total).Error; err != nil {
-			return nil, 0, err
-		}
-	
-		if offset > 0 {
-			query = query.Offset(int(offset))
-		}
-	
-		if limit > 0 {
-			query = query.Limit(int(limit))
-		}
-	
-		var invoices []entities.Invoice
-	
-		if err := query.Find(&invoices).Error; err != nil {
-			return nil, 0, err
-		}
+	if err := query.Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	if offset > 0 {
+		query = query.Offset(int(offset))
+	}
+
+	if limit > 0 {
+		query = query.Limit(int(limit))
+	}
+
+	var invoices []entities.Invoice
+
+	if err := query.Find(&invoices).Error; err != nil {
+		return nil, 0, err
+	}
 	dInvoices := mappers.InvoiceEntitiesToDomain(invoices)
 	return dInvoices, uint(total), nil
+}
+
+func (r *invoiceRepo) GetInvoiceByTicketID(ctx context.Context, ticketID uint) (*invoice.Invoice, error) {
+	var eI entities.Invoice
+
+	if err := r.db.WithContext(ctx).
+		Where("ticket_id = ?", ticketID).
+		First(&eI).Error; err != nil {
+		if strings.Contains(err.Error(), "record not found") {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("%w %w ticket id %d", invoice.ErrFailedToGetInvoice, err, ticketID)
+	}
+
+	dI := mappers.SimpleInvoiceEntityToDomain(eI)
+
+	return &dI, nil
+}
+
+func (r *invoiceRepo) UpdateInvoice(ctx context.Context, id uint, updates map[string]interface{}) error {
+	var t entities.Invoice
+
+	if err := r.db.WithContext(ctx).Model(&t).Updates(updates).Error; err != nil {
+		return fmt.Errorf("%w %w", invoice.ErrFailedToUpdate, err)
+	}
+
+	return nil
 }
