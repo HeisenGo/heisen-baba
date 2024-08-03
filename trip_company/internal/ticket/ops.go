1e8e10d5c0dae2cc67940dd554d8e3111c79e1bf
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 18:22:05 +0330

    feat: create and update ticket ops and types

--- trip_company/internal/ticket/ops.go
+++ trip_company/internal/ticket/ops.go
@@ -1,9 +1,21 @@
 package ticket
 
+import (
+	"context"
+)
+
 type Ops struct {
 	repo Repo
 }
 
 func NewOps(repo Repo) *Ops {
 	return &Ops{repo}
+}
+
+func (o *Ops) Create(ctx context.Context, t *Ticket) error {
+	return o.repo.Insert(ctx, t)
+}
+
+func (o *Ops) UpdateTicketStatus(ctx context.Context, ticketID uint, status string) error {
+	return o.repo.UpdateTicketStatus(ctx, ticketID, status)
 }
--- trip_company/internal/ticket/types.go
+++ trip_company/internal/ticket/types.go
@@ -1,18 +1,22 @@
 package ticket
 
-import "tripcompanyservice/internal/trip"
-
+import (
+	"context"
+	"tripcompanyservice/internal/trip"
+)
 
 type Repo interface {
+	Insert(ctx context.Context, t *Ticket) error
+    UpdateTicketStatus(ctx context.Context, ticketID uint, status string) error
 }
 
 type Ticket struct {
-    ID uint
-    TripID     uint            
-    Trip       trip.Trip         
-    UserID     *uint            // Use `default:NULL` for nullable field
-    AgencyID   *uint           
-    Quantity   int
-    TotalPrice float64
-    Status     string         
+	ID         uint
+	TripID     uint
+	Trip       *trip.Trip
+	UserID     *uint // Use `default:NULL` for nullable field
+	AgencyID   *uint
+	Quantity   int
+	TotalPrice float64
+	Status     string
 }
