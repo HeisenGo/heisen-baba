6125dba6b00d5b86bf5708fa3208e74339fb7f71
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 22:33:35 +0330

    feat: get tickets and cancel tickets handlers

--- trip_company/api/http/handlers/ticket.go
+++ trip_company/api/http/handlers/ticket.go
@@ -51,3 +51,102 @@ func BuyTicket(ticketService *service.TicketService) fiber.Handler { //serviceFa
 		return presenter.Created(c, "Ticket created successfully", res)
 	}
 }
+
+func GetTickets(ticketService *service.TicketService) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// userClaims, ok := c.Locals(UserClaimKey).(*jwt.UserClaims)
+		// if !ok {
+		// 	return SendError(c, errWrongClaimType, fiber.StatusBadRequest)
+		// }
+		//query parameter
+		page, pageSize := PageAndPageSize(c)
+		// get from auth!!!!! TODO:
+		// Parse dates
+		//var err error
+		//companyID, err := c.ParamsInt("companyID")
+		UserID := uint(1)
+		var data interface{}
+		if UserID != 0 { // TODO :
+
+			tickets, total, err := ticketService.GetTicketsByUserOrAgency(c.UserContext(), &UserID, nil, uint(page), uint(pageSize))
+			if err != nil {
+				//err := errors.New("Error")
+				return presenter.InternalServerError(c, err)
+			}
+			res := presenter.BatchTicketsToUserTickets(tickets)
+
+			data = presenter.NewPagination(
+				res,
+				uint(page),
+				uint(pageSize),
+				total,
+			)
+		} else {
+			agencyID := uint(1)
+			tickets, total, err := ticketService.GetTicketsByUserOrAgency(c.UserContext(), nil, &agencyID, uint(page), uint(pageSize))
+			if err != nil {
+				//err := errors.New("Error")
+				return presenter.InternalServerError(c, err)
+			}
+			res := presenter.BatchTicketsToUserTickets(tickets)
+
+			data = presenter.NewPagination(
+				res,
+				uint(page),
+				uint(pageSize),
+				total,
+			)
+		}
+
+		return presenter.OK(c, "Tickets fetched successfully", data)
+	}
+}
+
+func CancelTripByID(ticketService *service.TicketService) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// userClaims, ok := c.Locals(UserClaimKey).(*jwt.UserClaims)
+		// if !ok {
+		// 	return SendError(c, errWrongClaimType, fiber.StatusBadRequest)
+		// }
+		//query parameter
+		ticketID, err := c.ParamsInt("ticketID")
+		if err != nil {
+			return presenter.BadRequest(c, errWrongIDType)
+		}
+
+		if ticketID < 0 {
+			return presenter.BadRequest(c, errWrongIDType)
+		}
+		// User ID is needed!!! TODO!!
+
+		var body map[string]interface{}
+		if err := c.BodyParser(&body); err != nil {
+			return presenter.BadRequest(c, fmt.Errorf("Invalid req body"))
+		}
+		var res interface{}
+
+		if _, ok := body["agency_id"]; ok {
+
+			var req *presenter.CancelTicket
+			if err := json.Unmarshal(c.Body(), &req); err != nil {
+				return presenter.BadRequest(c, fmt.Errorf("Invalid req body"))
+			}
+
+			invoice, err := ticketService.CancelTicket(c.UserContext(), ticketID, nil, req.AgencyID)
+			if err != nil { //err = "failed to process agency ticket"
+				return presenter.BadRequest(c, err)
+			}
+			res = presenter.InvoiceToAgencyInvoice(*invoice)
+		} else {
+			// USER ID is needed ! TODO :
+			userID := uint(1)
+			invoice, err := ticketService.CancelTicket(c.UserContext(), ticketID, &userID, nil)
+			if err != nil { //err = "failed to process agency ticket"
+				return presenter.BadRequest(c, err)
+			}
+			res = presenter.InvoiceToAgencyInvoice(*invoice)
+		}
+
+		return presenter.OK(c, "Ticket Canceled successfully", res)
+	}
+}
