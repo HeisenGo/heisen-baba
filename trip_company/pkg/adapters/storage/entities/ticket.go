4cc538f9a7372a885f8884a95916ead8af69bb79
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 18:23:47 +0330

    fix: trip is now a pointer

--- trip_company/pkg/adapters/storage/entities/ticket.go
+++ trip_company/pkg/adapters/storage/entities/ticket.go
@@ -5,10 +5,10 @@ import "gorm.io/gorm"
 type Ticket struct {
     gorm.Model
     TripID     uint            `gorm:"not null"`
-    Trip       Trip            `gorm:"foreignKey:TripID"`
+    Trip       *Trip            `gorm:"foreignKey:TripID"`
     UserID     *uint           `gorm:"default:NULL"` // Use `default:NULL` for nullable field
     AgencyID   *uint           `gorm:"default:NULL"`
     Quantity   int
     TotalPrice float64
-    Status     string          `gorm:"type:varchar(20);default:'pending'"`
+    Status     string          `gorm:"type:varchar(20);default:'pending'"` //confirmed
 }
