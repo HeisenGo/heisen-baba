391c2f3ce249034ee7f7b2d2764e3a46d757ed2c
Neda Hn <z.hosseini.7@gmail.com>
2024-08-03 18:23:20 +0330

    fix: added status for invoice

--- trip_company/pkg/adapters/storage/entities/invoice.go
+++ trip_company/pkg/adapters/storage/entities/invoice.go
@@ -8,10 +8,11 @@ import (
 
 type Invoice struct {
 	gorm.Model
-	TicketID   uint      `gorm:"not null"`
-	Ticket     Ticket    `gorm:"foreignKey:TicketID"`
-	IssuedDate time.Time `gorm:"not null"`
-	Info       string    `gorm:"type:text"` // Detailed information for the invoice
+	TicketID       uint      `gorm:"not null"`
+	Ticket         *Ticket    `gorm:"foreignKey:TicketID"`
+	IssuedDate     time.Time `gorm:"not null"`
+	Info           string    `gorm:"type:text"` // Detailed information for the invoice
 	PerAmountPrice float64
-	TotalPrice 
+	TotalPrice     float64
+	Status         string `gorm:"type:varchar(20);default:'pending'"` // paid
 }
