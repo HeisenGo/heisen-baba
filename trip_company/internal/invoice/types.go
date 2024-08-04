d110440b63727a342298c9c21bc3b5151a7f67e1
Neda Hn <z.hosseini.7@gmail.com>
2024-08-04 22:39:24 +0330

    feat: calculate profit for a trip

--- trip_company/internal/invoice/types.go
+++ trip_company/internal/invoice/types.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"time"
-	"tripcompanyservice/internal/ticket"
 )
 
 var(
@@ -20,24 +19,25 @@ type Repo interface {
 	Insert(ctx context.Context, i *Invoice) error
 	UpdateInvoiceStatus(ctx context.Context, invoiceID uint, status string) error
 	UpdateInvoice(ctx context.Context, id uint, updates map[string]interface{}) error
+	CalculateCompanyProfitForTrip(ctx context.Context, tripID uint) (float64, error)
+
 }
 
 type Invoice struct {
 	ID             uint
 	TicketID       uint
-	Ticket         *ticket.Ticket
 	IssuedDate     time.Time
 	Info           string // Detailed information for the invoice
 	PerAmountPrice float64
 	TotalPrice     float64
 	Status         string
 	Penalty     float64
+
 }
 
-func NewInvoice(ticket_id uint, ticket *ticket.Ticket, issuedDate time.Time, info string, perAmountPrice, totalPrice, penalty float64) *Invoice {
+func NewInvoice(ticket_id uint, issuedDate time.Time, info string, perAmountPrice, totalPrice, penalty float64) *Invoice {
 	return &Invoice{
 		TicketID:       ticket_id,
-		Ticket:         ticket,
 		IssuedDate:     issuedDate,
 		Info:           info,
 		PerAmountPrice: perAmountPrice,
