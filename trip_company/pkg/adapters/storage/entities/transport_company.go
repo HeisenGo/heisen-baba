dc470ba663b868b0d933dba6ba08725e529bc289
Neda Hn <z.hosseini.7@gmail.com>
2024-08-04 20:18:59 +0330

    feat: entities updated

--- trip_company/pkg/adapters/storage/entities/transport_company.go
+++ trip_company/pkg/adapters/storage/entities/transport_company.go
@@ -14,7 +14,7 @@ type TransportCompany struct {
 	//Email       string `gorm:"type:varchar(100);uniqueIndex:idx_phone_email"`
 	IsBlocked   bool   `gorm:"not null;default:false"`
 	// relationships
-	Employees []Employee `gorm:"foreignKey:TransportCompanyID"`
-	Trips     []Trip     `gorm:"foreignKey:TransportCompanyID"`
-	TechTeams []TechTeam `gorm:"foreignKey:TransportCompanyID"`
+	Employees []Employee `gorm:"foreignKey:TransportCompanyID; constraint:OnDelete:CASCADE;"`
+	Trips     []Trip     `gorm:"foreignKey:TransportCompanyID; constraint:OnDelete:CASCADE;"`
+	TechTeams []TechTeam `gorm:"foreignKey:TransportCompanyID; constraint:OnDelete:CASCADE;"`
 }
