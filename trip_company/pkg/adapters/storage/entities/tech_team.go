5ba39607443269ef5edd009c875382867f7f3eb0
Neda Hn <z.hosseini.7@gmail.com>
2024-08-04 20:18:24 +0330

    fix: entities updated

--- trip_company/pkg/adapters/storage/entities/tech_team.go
+++ trip_company/pkg/adapters/storage/entities/tech_team.go
@@ -4,18 +4,19 @@ import "gorm.io/gorm"
 
 type TechTeam struct {
 	gorm.Model
-	Name               string   `gorm:"type:varchar(100);not null"`
-	Description        string   `gorm:"type:text"`
+	Name               string `gorm:"type:varchar(100);not null"`
+	Description        string `gorm:"type:text"`
 	TripType           string `gorm:"type:varchar(20);not null"`
 	TransportCompanyID uint
 	TransportCompany   TransportCompany `gorm:"foreignKey:TransportCompanyID"`
-	Members            []TechTeamMember `gorm:"foreignKey:TechTeamID"`
+	Members            []TechTeamMember `gorm:"foreignKey:TechTeamID;constraint:OnDelete:CASCADE;"`
 }
 
 type TechTeamMember struct {
 	gorm.Model
-	TechTeamID uint
-	TechTeam   TechTeam `gorm:"foreignKey:TechTeamID"`
-	UserID     uint   `gorm:"not null"`
-	Role       string `gorm:"type:varchar(50)"`
+	TechTeamID uint     `gorm:"not null; uniqueIndex:idx_tech_team_user"`
+	TechTeam   TechTeam `gorm:"foreignKey:TechTeamID; constraint:OnDelete:CASCADE;"`
+	UserID     uint     `gorm:"not null; uniqueIndex:idx_tech_team_user"`
+	Role       string   `gorm:"type:varchar(50); default:'technician'"`
+	Email      string
 }
