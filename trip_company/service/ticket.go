9a38941b0a7605b4f6b110d36b12a814ad483cc7
Neda Hn <z.hosseini.7@gmail.com>
2024-08-06 20:27:19 +0330

    feat: agency buy ticket

--- trip_company/service/ticket.go
+++ trip_company/service/ticket.go
@@ -31,38 +31,45 @@ func NewTicketService(ticketOps *ticket.Ops, tripOps *trip.Ops, invoiceOps *invo
 }
 
 func (s *TicketService) ProcessAgencyTicket(ctx context.Context, t *ticket.Ticket) error {
-	// 	trip, err := s.tripOps.GetFullTripByID(ctx, t.TripID)
-	// 	if err != nil {
-	// 		return err
-	// 	}
+	trp, err := s.tripOps.GetFullTripByID(ctx, t.TripID)
+	if err != nil {
+		return err
+	}
+	t.Trip = trp
+	// check agency exist !!! TODO
+	// get agency owner wallet!
+	if trp.TourReleaseDate.After(time.Now()) {
+		return ErrImpossibleToBuy
+	}
+	if trp.SoldTickets+uint(t.Quantity) > trp.MaxTickets {
+		return ErrImpossibleToBuy
+	}
+	t.TotalPrice = trp.AgencyPrice * float64(t.Quantity)
+	// t.UserID agency owner ID
+	err = s.ticketOps.Create(ctx, t)
+	if err != nil {
+		return err
+	}
+	info := fmt.Sprintf("Agency %v bought %d trips of from %s %s to %s %s in %s for date %v", t.AgencyID, t.Quantity, trp.Origin, trp.Path.FromTerminal.Name, trp.Destination, trp.Path.ToTerminal.Name, trp.TripType, trp.StartDate)
 
-	// 	// check agency exist !!! TODO
-	// 	// get agency owner wallet!
-	// 	if trip.TourReleaseDate.After(time.Now()) {
-	// 		return ErrImpossibleToBuy
-	// 	}
-	// 	t.TotalPrice = trip.AgencyPrice * float64(t.Quantity)
-	// 	// t.UserID agency owner ID
-	// 	err = s.ticketOps.CreateTicket(ctx, t)
-	// 	agency_id := 1
-	// 	info := fmt.Sprintf("Agency %s bought %d trips of from %s %s to %s %s in %s for date %v", agency_id, t.Quantity, trip.Origin, trip.Path.FromTerminal.Name, trip.Destination, trip.Path.ToTerminal.Name, trip.TripType, trip.StartDate)
+	newInvoice := invoice.NewInvoice(t.ID, time.Now(), info, trp.AgencyPrice, t.TotalPrice, 0)
 
-	// 	newInvoice := invoice.NewInvoice(t.ID, t, time.Now(), info, trip.AgencyPrice, t.TotalPrice)
+	err = s.invoiceOps.Create(ctx, newInvoice)
+	t.Invoice = *newInvoice
+	if err != nil {
+		return err
+	}
 
-	// 	s.invoiceOps.CreateInvoice(ctx, newInvoice)
-
-	// 	//if invoice was successfull // TODO
-	// 	//****************************************
-	// 	trip.SoldTickets = trip.SoldTickets + float64(t.Quantity)
-	// 	newTrip := trip.NewTripTOUpdateSoldTickets(trip.SoldTickets)
-	// 	s.tripOps.UpdateTrip(ctx, trip.ID, newTrip, trip)
-	// 	s.ticketOps.UpdateTicket()
-	// 	s.invoiceOps.UpdateInvoice()
-	// 	// notif
-	// 	return nil
-	// }
-
-	// func (s *TicketService) ProcessUserTicket(ctx context.Context, t *ticket.Ticket) error {
+	//if invoice was successfull // TODO
+	//****************************************
+	trp.SoldTickets = trp.SoldTickets + uint(t.Quantity)
+	newTrip := trip.NewTripTOUpdateSoldTickets(trp.SoldTickets)
+	s.tripOps.UpdateTrip(ctx, trp.ID, newTrip, trp)
+	t.Status = "Paid"
+	newInvoice.Status = "Paid"                                   //sold tickets
+	s.ticketOps.UpdateTicketStatus(ctx, t.ID, "Paid")            //state
+	s.invoiceOps.UpdateInvoiceStatus(ctx, newInvoice.ID, "Paid") //state
+	// notif with bank?
 	return nil
 }
 
