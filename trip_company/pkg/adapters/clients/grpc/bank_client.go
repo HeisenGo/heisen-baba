279e4d686ea652457494f8a1860a59b9a3bc5fd1
Neda Hn <z.hosseini.7@gmail.com>
2024-08-07 12:28:38 +0330

    feat: add bank service

---
+++ trip_company/pkg/adapters/clients/grpc/bank_client.go
@@ -0,0 +1,71 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"tripcompanyservice/internal/bank"
+	"tripcompanyservice/pkg/ports"
+	"tripcompanyservice/protobufs"
+)
+
+type GRPCBankClient struct {
+	ServiceRegistry ports.IServiceRegistry
+	BankServiceName string
+}
+
+func NewGRPCBankClient(serviceRegistry ports.IServiceRegistry, bankServiceName string) *GRPCBankClient {
+	return &GRPCBankClient{ServiceRegistry: serviceRegistry, BankServiceName: bankServiceName}
+}
+
+func (g *GRPCBankClient) Transfer(senderOwnerID, receiverOwnerID string, isPaidToSystem bool, amount uint64) (bool, error) {
+	port, ip, err := g.ServiceRegistry.DiscoverService(g.BankServiceName)
+	if err != nil {
+		return false, err
+	}
+
+	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", ip, port), grpc.WithInsecure())
+	if err != nil {
+		return false, err
+	}
+
+	defer conn.Close()
+
+	// Create a new AuthService client
+	client := protobufs.NewBankServiceClient(conn)
+
+	// Create a context
+	ctx := context.Background()
+
+	// Prepare the request
+	request := &protobufs.TransferRequest{
+		SenderOwnerID:   "ecfe4a77-a1c3-4dd9-abfc-3349bd4b9db2",
+		ReceiverOwnerID: "ecfe4a77-a1c3-4dd9-abfc-3349bd4b9db2",
+		IsPaidToSystem:  false,
+		Amount:          50000,
+	}
+
+	// Call the GetUserByToken method
+	// Call the Transfer method
+	response, err := client.Transfer(ctx, request)
+	if err != nil {
+		return false, err
+	}
+
+	// Extract the gRPC status code
+	st, ok := status.FromError(err)
+	if !ok {
+		return false, err
+	}
+	switch st.Code() {
+	case codes.OK:
+		if response.Status == "success" {
+			return true, nil
+		}
+	default:
+		return false, bank.ErrNotEnoughMoney
+	}
+	return false, nil
+}
--- trip_company/pkg/ports/clients/clients/bank_client.go
+++
@@ -1,6 +0,0 @@
-package clients
-
-
-type IBankClient interface {
-	Transfer(senderOwnerID, receiverOwnerID, isPaidToSystem string, amount uint64) (bool,error)
-}
